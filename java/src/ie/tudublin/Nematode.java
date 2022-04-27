0da9eefebc9d7685cfe2f15f097e62a47ab497ec
Eoin <eoinot128@gmail.com>
2022-04-27 18:54:07 +0100

    Finishing gender female and h and adding colour changing and changing text size since it was small

--- java/src/ie/tudublin/Nematode.java
+++ java/src/ie/tudublin/Nematode.java
@@ -83,20 +83,23 @@ public class Nematode {
         float halfH = nt.height/2;
 		float halfW = nt.width/2;
         float halfL = length/2;
+
+        float colour = PApplet.map(nt.i,0,nt.nematodes.size()-1,0,255);
         
-    
         nt.background(0);
         
-        nt.stroke(255,255,255);
+        nt.stroke(colour,255,255);
 
-        nt.fill(255,255,255);
+        nt.fill(colour,255,255);
         nt.textAlign(PApplet.CENTER, PApplet.CENTER);
+        nt.textSize(40);
         nt.text(name, halfW, (halfH - (halfL * 40) - 100));
 
         nt.noFill();
         
         for(int nem_leng = 0; nem_leng < length; nem_leng++)
         {
+          
             nt.pushMatrix();
             nt.translate(halfH, (halfH - (halfL * 40)) + (50 * nem_leng));
             nt.circle(0, 0, 50);
